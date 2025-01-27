c0bba985f6a43e4333e66b4faa4b668c43d8a786
Pablors2109 <mrpablo219@gmail.com>
2025-01-27 17:30:31 +0100

    experimento con gitignore

---
+++ config_sample.php
@@ -0,0 +1,10 @@
+<?php
+define(constant_name:"HOST", value:"direccion del servidor");
+define(constant_name:"DB_USER", value:"usuario bbdd");
+define(constant_name:"DB_PASS", value:"password bbdd");
+define(constant_name:"DB_DATABASE", value:"bbdd");
+?>
+
+
+
+
